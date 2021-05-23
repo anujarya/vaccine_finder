b8258ec4556002c58e07628ceb056b704a0e4510
Anuj <anuj.arya.mca@gmail.com>
2021-05-23 17:57:09 +0530

    Modified to accomodate dose1 and dose2

--- lib/MyCowinApi.py
+++ lib/MyCowinApi.py
@@ -1,17 +1,73 @@
-from cowin_api.api import CoWinAPI
-
 from lib.MyConstants import MyConstants
-from typing import Union
-
 import requests
 from fake_useragent import UserAgent
 from requests.exceptions import HTTPError
 import logging
 
+from typing import Union, List
 
-class MyCoWinAPI(CoWinAPI):
+from cowin_api.base_api import BaseApi
+from cowin_api.utils import today
+
+
+class MyCoWinAPI(BaseApi):
     logging.basicConfig(filename='vaccine_finder.log', filemode='w', format='%(asctime)s - %(message)s',
                         level=logging.DEBUG)
+
+    def get_states(self):
+        url = MyConstants.states_list_url
+        return self._call_api(url)
+
+    def get_districts(self, state_id: str):
+        url = f"{MyConstants.districts_list_url}/{state_id}"
+        return self._call_api(url)
+
+    def filter_centers(self, centers: dict, filters: dict):
+        original_centers = centers.get('centers')
+        filtered_centers = {'centers': []}
+        for index, center in enumerate(original_centers):
+            filtered_sessions = []
+            for session in center.get('sessions'):
+                matched = True
+                for key in filters.keys():
+                    matched = matched and ((not filters.get(key)) or session.get(key) == filters.get(key))
+                if matched:
+                    filtered_sessions.append(session)
+            if len(filtered_sessions) > 0:
+                center['sessions'] = filtered_sessions
+                filtered_centers['centers'].append(center)
+        return filtered_centers
+
+    def get_availability_by_base(self, caller: str, areas: Union[str, List[str]], date: str, filters: dict):
+        """this function is called by the get availability function
+        this is separated out so that the parent functions have the same
+        structure and development becomes easier"""
+        area_type, base_url = 'pincode', MyConstants.availability_by_pin_code_url
+        if caller == 'district':
+            area_type, base_url = 'district_id', MyConstants.availability_by_district_url
+        # if the areas is a str, convert to list
+        if isinstance(areas, str):
+            areas = [areas]
+        # make a separate call for each of the areas
+        results = []
+        for area_id in areas:
+            url = f"{base_url}?{area_type}={area_id}&date={date}"
+            if filters:
+                curr_result = self.filter_centers(self._call_api(url), filters)
+            else:
+                curr_result = self._call_api(url)
+            # append
+            if curr_result:
+                results += curr_result['centers']
+
+        # return the results in the same format as returned by the api
+        return {'centers': results}
+
+    def get_availability_by_pincode(self, pin_code: Union[str, List[str]],
+                                    date: str = today(),
+                                    filters: dict = {}):
+        return self.get_availability_by_base(caller='pincode', areas=pin_code,
+                                             date=date, filters=filters)
 
     def post_api(self, url, data) -> Union[HTTPError, dict]:
         user_agent = UserAgent()
@@ -24,13 +80,13 @@ class MyCoWinAPI(CoWinAPI):
 
         return response.json()
 
-    def get_available_slots(self, pin_code, date, min_age_limit):
-        available_centers = CoWinAPI.get_availability_by_pincode(self, pin_code, date, min_age_limit)
+    def get_available_slots(self, pin_code, date, dose_key, filters: dict):
+        available_centers = self.get_availability_by_pincode(pin_code, date, filters)
         filtered_centers = []
         for index, center in enumerate(available_centers.get("centers")):
             logging.info(f"Scanning center:{center}\n")
             for session in center.get("sessions"):
-                if session.get("available_capacity") > 0:
+                if session.get(dose_key) > 0:
                     filtered_centers.append({
                         "center_id": center.get("center_id"),
                         "name": center.get("name"),
@@ -41,11 +97,14 @@ class MyCoWinAPI(CoWinAPI):
                         "session_id": session.get("session_id"),
                         "date": session.get("date"),
                         "available_capacity": session.get("available_capacity"),
-                        "min_age_limit": session.get("min_age_limit"),
+                        "available_capacity_dose1": session.get("available_capacity_dose1"),
+                        "available_capacity_dose2": session.get("available_capacity_dose2"),
+                        "min_age_limt": session.get("min_age_limt"),
                         "vaccine": session.get("vaccine")
                     })
         return filtered_centers
 
-    def available_vaccination_center(self, pin_code, date, min_age_limit) -> int:
-        available_centers = CoWinAPI.get_availability_by_pincode(self, pin_code, date, min_age_limit)
+    def available_vaccination_center_by_age(self, pin_code, date, filters: dict) -> int:
+
+        available_centers = self.get_availability_by_pincode(pin_code, date, filters)
         return len(available_centers.get("centers"))
--- vaccine_finder.py
+++ vaccine_finder.py
@@ -1,11 +1,11 @@
 from lib.MyConstants import MyConstants
 from lib.MyCowinApi import MyCoWinAPI
-from io import StringIO
-import csv
 import time
 import winsound
 import logging
 from datetime import datetime
+import click
+
 
 def create_logger():
     logger = logging.getLogger(__name__)
@@ -16,7 +16,7 @@ def create_logger():
     f_handler.setLevel(logging.INFO)
 
     # Create formatters and add it to handlers
-    c_format = logging.Formatter('%(asctime)s - %(message)s')
+    c_format = logging.Formatter('%(message)s')
     f_format = logging.Formatter('%(asctime)s - %(message)s')
     c_handler.setFormatter(c_format)
     f_handler.setFormatter(f_format)
@@ -27,30 +27,48 @@ def create_logger():
     return logger
 
 
-if __name__ == '__main__':
+@click.command()
+@click.option('--pin', prompt='Pin Code(CSV)', default="201001", help='Specify the pin code you want to search for.')
+@click.option('--age', prompt='Age(18, 45, both)', default="18", help='Minimum age to find the vaccine.')
+@click.option('--dose', prompt='Dose Number(1 or 2, default is 1)', default=1, help='Dose number you are looking for.',
+              required=True)
+@click.option('--vaccine', prompt='Vaccine Name(default is all)', default="all",
+              help='Name of the vaccine you are looking for.')
+def main(pin, age, vaccine, dose):
     log = create_logger()
-    input_pin_codes = input("Enter pin code: ")
-    if input_pin_codes:
-        pin_codes = [input_pin_codes]
+    pin_codes = pin.split(',')
+
+    if vaccine == "all":
+        vaccine = None
+    if age == "both":
+        min_age_limit = None
     else:
-        pin_codes = ["201001"]
-    min_age_limit = int(input("Minimum age limit(18 or 45): "))
+        min_age_limit = int(age)
+
     date = datetime.today().strftime(MyConstants.DD_MM_YYYY)
     cowin = MyCoWinAPI()
 
+    slot_filters = {
+        "min_age_limit": min_age_limit,
+        "vaccine": vaccine,
+    }
+
+    dose_key = "available_capacity_dose1" if dose == 1 else "available_capacity_dose2"
+
     while 1:
         for pin_code in pin_codes.copy():
-
-            if cowin.available_vaccination_center(pin_code, date=date, min_age_limit=min_age_limit) == 0:
+            if cowin.available_vaccination_center_by_age(pin_code, date, {"min_age_limit": min_age_limit}) == 0:
                 log.info(f"There is no vaccination center available for pin code:{pin_code}\n")
                 break
 
-            available_centers = cowin.get_available_slots(pin_code, date, min_age_limit)
+            available_centers = cowin.get_available_slots(pin_code, date, dose_key, slot_filters)
 
-            log.info(f"List of centers with available Vaccine Slots :\n")
+            log.info(f"List of centers with available Vaccine Slots for pin code:{pin_code}:\n")
             for index, center in enumerate(available_centers):
                 log.info(
-                    f"Date :{center.get('date')}, available_capacity :{center.get('available_capacity')}, pin code :{center.get('pincode')}, Center Name:{center.get('name')}, vaccine : {center.get('vaccine')}\n")
+                    f"Date:{center.get('date')}, Available Dose 1 Slots:{center.get('available_capacity_dose1')}, "
+                    f"Available Dose 2 Slots:{center.get('available_capacity_dose2')}, Center Name: {center.get('name')}, "
+                    f"Pin Code:{center.get('pincode')}, Vaccine: {center.get('vaccine')}")
                 duration = 100  # milliseconds
                 freq = 1840  # Hz
                 winsound.Beep(freq, duration)
@@ -59,3 +77,7 @@ if __name__ == '__main__':
 
         log.info(f"sleeping for 10 seconds.....")
         time.sleep(10)
+
+
+if __name__ == '__main__':
+    main()
